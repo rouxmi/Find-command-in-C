5d78ae058437558cb2a28901a14d5d159a57c405
Bourdais Rémi <remi.bourdais@telecomnancy.eu>
2022-12-14 10:17:01 +0100

    first test

--- src/ftc.c
+++ src/ftc.c
@@ -1,17 +1,6 @@
 #include "../includes/ftc.h"
 
 int main(int argc,char *argv[ ]) {
-    // check sil y a assez darguments
-    if (argc < 2) {
-        fprintf(stderr,"Necessite au moins un pwd \n");
-        exit(1);
-    }
-    
-    if (argc == 2) {
-        //Pas de parametre de recherche
-        //listFilesRecursively(argv[1]);
-        exit(0);
-    }
     
     return 0;
 }
