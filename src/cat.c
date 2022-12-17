0af77db4945141d629454e53191c3a37b4a0c771
Bourdais Rémi <remi.bourdais@telecomnancy.eu>
2022-12-17 10:52:36 +0100

    find the cat

--- src/cat.c
+++ src/cat.c
@@ -1,15 +1,26 @@
 #include "../includes/cat.h"
 
+bool testctc(char* file_path,char* str) {
+  // Open the file
+  FILE* file = fopen(file_path, "r");
 
-bool testctc(char* path, char* ctc){
-    FILE* ptr=fopen(path,"r");
-    char filectc[1000];
-    fgets(filectc,1000,ptr);
-    printf("%s \n",filectc);
-    if (strstr(filectc,ctc) != NULL){
-        fclose(ptr);
-        return true;
-    }
-    fclose(ptr);
+  // Check if the file was successfully opened
+  if (file == NULL) {
     return false;
+  }
+
+  // Read the file line by line
+  char line[1024];
+  while (fgets(line, sizeof(line), file) != NULL) {
+    // Check if the string is present in the current line
+    if (strstr(line, str) != NULL) {
+      // Close the file and return 1 if the string is found
+      fclose(file);
+      return true;
+    }
+  }
+
+  // Close the file and return 0 if the string is not found
+  fclose(file);
+  return false;
 }
